642314ebb93fb116ba531827548735186a0e41bf
Sakhil-N-Maju <sakhil.n.maju123@gmail.com>
2025-03-23 10:36:10 +0530

    Update bankers.c

--- OS/bankers.c
+++ OS/bankers.c
@@ -2,127 +2,108 @@
 #include <stdbool.h>
 
 void calculateNeed(int P, int R, int need[P][R], int max[P][R], int allot[P][R]) {
-    for (int i = 0; i < P; i++) {
-        for (int j = 0; j < R; j++) {
+    for (int i = 0; i < P; i++)
+        for (int j = 0; j < R; j++)
             need[i][j] = max[i][j] - allot[i][j];
-        }
-    }
 }
 
 bool isSafe(int P, int R, int processes[], int avail[], int max[][R], int allot[][R], int safeSeq[]) {
-    int need[P][R];
+    int need[P][R], work[R], finish[P];
+    for (int i = 0; i < P; i++)
+        finish[i] = 0;
+
     calculateNeed(P, R, need, max, allot);
-    
-    bool finish[P];
-    for (int i = 0; i < P; i++) finish[i] = false;
-    int work[R];
-    for (int i = 0; i < R; i++)
-        work[i] = avail[i];
-    
+
+    for (int j = 0; j < R; j++) 
+        work[j] = avail[j];
+
     int count = 0;
     while (count < P) {
         bool found = false;
-        for (int p = 0; p < P; p++) {
-            if (!finish[p]) {
-                int j;
-                for (j = 0; j < R; j++)
-                    if (need[p][j] > work[j])
+
+        for (int i = 0; i < P; i++) {
+            if (!finish[i]) {
+                bool canExecute = true;
+                for (int j = 0; j < R; j++) {
+                    if (need[i][j] > work[j]) {
+                        canExecute = false;
                         break;
-                
-                if (j == R) {
-                    for (int k = 0; k < R; k++)
-                        work[k] += allot[p][k];
-                    safeSeq[count++] = processes[p];
-                    finish[p] = true;
+                    }
+                }
+                if (canExecute) {
+                    for (int j = 0; j < R; j++) 
+                        work[j] += allot[i][j];
+
+                    safeSeq[count++] = processes[i];
+                    finish[i] = 1;
                     found = true;
                 }
             }
         }
-        
-        if (!found) {
-            return false;
-        }
+        if (!found) return false;
     }
     return true;
 }
 
-bool requestResources(int P, int R, int process, int request[], int avail[], int max[P][R], int allot[P][R]) {
-    int need[P][R];
-    calculateNeed(P, R, need, max, allot);
-    
-    for (int i = 0; i < R; i++) {
-        if (request[i] > need[process][i] || request[i] > avail[i]) {
-            printf("Request cannot be granted immediately.\n");
+bool canGrantRequest(int P, int R, int avail[], int need[P][R], int request[], int processID) {
+    for (int j = 0; j < R; j++) {
+        if (request[j] > need[processID][j] || request[j] > avail[j]) 
             return false;
-        }
     }
-    
-    for (int i = 0; i < R; i++) {
-        avail[i] -= request[i];
-        allot[process][i] += request[i];
-        need[process][i] -= request[i];
-    }
-    
-    int safeSeq[P];
-    if (!isSafe(P, R, (int[]){0, 1, 2, 3, 4}, avail, max, allot, safeSeq)) {
-        for (int i = 0; i < R; i++) {
-            avail[i] += request[i];
-            allot[process][i] -= request[i];
-            need[process][i] += request[i];
-        }
-        printf("Request cannot be granted as it leads to an unsafe state.\n");
-        return false;
-    }
-    printf("Request can be granted.\n");
     return true;
 }
 
 int main() {
     int P, R;
+    
     printf("Enter number of processes: ");
     scanf("%d", &P);
     printf("Enter number of resources: ");
     scanf("%d", &R);
-    
-    int processes[P];
-    for (int i = 0; i < P; i++)
+
+    int processes[P], avail[R], max[P][R], allot[P][R], safeSeq[P];
+
+    for (int i = 0; i < P; i++) 
         processes[i] = i;
-    
-    int available[R];
+
     printf("Enter available resources: ");
-    for (int i = 0; i < R; i++)
-        scanf("%d", &available[i]);
-    
-    int max[P][R], allocation[P][R];
+    for (int j = 0; j < R; j++) 
+        scanf("%d", &avail[j]);
+
     printf("Enter max resource matrix:\n");
-    for (int i = 0; i < P; i++)
-        for (int j = 0; j < R; j++)
+    for (int i = 0; i < P; i++) 
+        for (int j = 0; j < R; j++) 
             scanf("%d", &max[i][j]);
-    
-    printf("Enter allocation matrix:\n");
-    for (int i = 0; i < P; i++)
-        for (int j = 0; j < R; j++)
-            scanf("%d", &allocation[i][j]);
-    
-    int safeSeq[P];
-    if (isSafe(P, R, processes, available, max, allocation, safeSeq)) {
-        printf("System is in a safe state. Safe sequence is: ");
-        for (int i = 0; i < P; i++)
-            printf("%d ", safeSeq[i]);
-        printf("\n");
+
+    printf("Enter allocated resource matrix:\n");
+    for (int i = 0; i < P; i++) 
+        for (int j = 0; j < R; j++) 
+            scanf("%d", &allot[i][j]);
+
+    if (isSafe(P, R, processes, avail, max, allot, safeSeq)) {
+        printf("\nSystem is in a SAFE state.\nSafe sequence: ");
+        for (int i = 0; i < P; i++) 
+            printf("P%d ", safeSeq[i]);
     } else {
-        printf("System is not in a safe state.\n");
+        printf("\nSystem is in an UNSAFE state. Deadlock is possible.\n");
     }
-    
-    int process;
-    printf("Enter process number making request: ");
-    scanf("%d", &process);
-    int request[R];
-    printf("Enter resource request for process %d: ", process);
-    for (int i = 0; i < R; i++)
-        scanf("%d", &request[i]);
-    
-    requestResources(P, R, process, request, available, max, allocation);
-    
+
+    int processID;
+    printf("\nEnter process ID for resource request: ");
+    scanf("%d", &processID);
+
+    int request[R], need[P][R];
+    calculateNeed(P, R, need, max, allot);
+
+    printf("Enter resource request for process P%d: ", processID);
+    for (int j = 0; j < R; j++) 
+        scanf("%d", &request[j]);
+
+    if (canGrantRequest(P, R, avail, need, request, processID)) {
+        printf("The request CAN be granted immediately.\n");
+    } else {
+        printf("The request CANNOT be granted immediately.\n");
+    }
+
     return 0;
 }
